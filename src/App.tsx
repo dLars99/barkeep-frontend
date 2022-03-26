fa3415e450b6a3b0b7ac1a8959b0835844c1c71d
David Larsen <davidlarsen@gmail.com>
2022-03-25 19:41:31 -0500

    feat(drinks): Add drink list route

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./views/Homepage";
 import CreateDrink from "./views/CreateDrink";
 import CreateIngredient from "./views/CreateIngredient";
+import DrinkList from "./views/DrinkList";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/drinks" element={<DrinkList />} />
         <Route path="/newdrink" element={<CreateDrink />} />
         <Route path="/newingredient" element={<CreateIngredient />} />
       </Routes>
