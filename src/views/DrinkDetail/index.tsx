9ac5bc7cced4898cf32538f988823a19941ecf41
dLars99 <davidlarsen@gmail.com>
2022-04-17 12:00:36 -0500

    feat(drink): Render all properties in drink detail

--- src/views/DrinkDetail/index.tsx
+++ src/views/DrinkDetail/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import CreateDrink from "../CreateDrink";
 import Button from "../../components/Button";
 import { Drink } from "../../types";
+import IngredientList from "../DrinkList/IngredientList";
 
 const DrinkDetail = ({ drink }: { drink: Drink }) => {
   const [edit, setEdit] = useState<boolean>(false);
@@ -14,6 +15,22 @@ const DrinkDetail = ({ drink }: { drink: Drink }) => {
         <div>
           <Button onClick={() => setEdit(true)}>Edit</Button>
           <h1>{drink.name}</h1>
+          <IngredientList ingredients={drink.ingredients} />
+          <h3>Instructions</h3>
+          <p>{drink.instructions}</p>
+          {drink.glass1 || drink.glass2 ? (
+            <p>
+              Glass:
+              {drink.glass1 ? <span>{` ${drink.glass1}`}</span> : null}
+              {drink.glass2 ? <span>{` ${drink.glass2}`}</span> : null}
+            </p>
+          ) : null}
+          {Number(drink.rating) ? (
+            <p>
+              Rating:
+              <span>{` ${drink.rating}`}</span>
+            </p>
+          ) : null}
         </div>
       )}
     </div>
