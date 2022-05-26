5b7343a50016e1bad55c39a8902ccd77eb200c11
dLars99 <davidplarsen@gmail.com>
2022-05-25 22:12:06 -0500

    feat(homepage): Add styling to homepage

--- src/views/Homepage/index.tsx
+++ src/views/Homepage/index.tsx
@@ -1,10 +1,22 @@
 import { createUseStyles } from "react-jss";
 import Button from "../../components/Button";
+import TikiBar from "../../assets/images/tikibar.jpg";
 
 const useStyles = createUseStyles({
   root: {
     padding: [0, 10],
     height: "100vh",
+    backgroundImage: `url(${TikiBar})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    boxSizing: "border-box",
+    paddingTop: 6,
+  },
+  title: {
+    fontFamily: "'Reggae One', cursive",
+    color: "#F2E30C",
+    marginTop: 0,
+    webKitbackgroundClip: "text",
   },
   buttonRow: {
     display: "flex",
@@ -14,10 +26,24 @@ const useStyles = createUseStyles({
     flex: "2",
     height: "40vh",
     marginRight: 10,
+    backgroundColor: "rgba(13, 0, 0, 0.4)",
+    color: "#F2E30C",
+    borderColor: "rgba(242, 227, 12, 0.6)",
+    borderRadius: 15,
+    fontFamily: "'Catamaran', sans-serif",
+    fontSize: 46,
+    fontWeight: 600,
   },
   rightButton: {
     flex: "1",
     height: "40vh",
+    backgroundColor: "rgba(13, 0, 0, 0.4)",
+    color: "#F2E30C",
+    borderColor: "rgba(242, 227, 12, 0.6)",
+    borderRadius: 15,
+    fontFamily: "'Catamaran', sans-serif",
+    fontSize: 46,
+    fontWeight: 600,
   },
 });
 
@@ -25,7 +51,7 @@ const Homepage = (): JSX.Element => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <h1>Barkeep</h1>
+      <h1 className={classes.title}>Barkeep</h1>
       <div className={classes.buttonRow}>
         <Button linkUrl="/drinks" className={classes.leftButton}>
           Find a Drink
