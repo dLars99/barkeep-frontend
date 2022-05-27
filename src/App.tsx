2ef11a0d034843e6564f717a513a1972595cd750
dLars99 <davidplarsen@gmail.com>
2022-05-26 19:34:52 -0500

    fix(homepage): Move background to root application

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createUseStyles } from "react-jss";
+import TikiBar from "./assets/images/tikibar.jpg";
+
+// Route targets
 import Homepage from "./views/Homepage";
 import CreateDrink from "./views/CreateDrink";
 import CreateIngredient from "./views/CreateIngredient";
 import DrinkList from "./views/DrinkList";
 
+const useStyles = createUseStyles({
+  mainApp: {
+    backgroundImage: `url(${TikiBar})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  },
+});
+
 function App() {
+  const classes = useStyles();
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/drinks" element={<DrinkList />} />
-        <Route path="/ingredients" element={<DrinkList byIngredients />} />
-        <Route path="/newdrink" element={<CreateDrink />} />
-        <Route path="/newingredient" element={<CreateIngredient />} />
-      </Routes>
-    </Router>
+    <div className={classes.mainApp}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/drinks" element={<DrinkList />} />
+          <Route path="/ingredients" element={<DrinkList byIngredients />} />
+          <Route path="/newdrink" element={<CreateDrink />} />
+          <Route path="/newingredient" element={<CreateIngredient />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
