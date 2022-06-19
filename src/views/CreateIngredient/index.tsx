794b0effad905d23f5c87fe48e0343dae80645dd
David Larsen <54251395+dLars99@users.noreply.github.com>
2022-06-18 23:47:53 -0500

    feat(styles): Style add ingredient form (#13)
    * feat(styles): Style form and inputs
    
    * feat(styles): Style submit button

--- src/views/CreateIngredient/index.tsx
+++ src/views/CreateIngredient/index.tsx
@@ -1,28 +1,37 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import axios, { AxiosError } from "axios";
-import { Formik, Form, Field, ErrorMessage, FormikErrors } from "formik";
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+  FormikErrors,
+  FormikProps,
+} from "formik";
 import { createUseStyles } from "react-jss";
 import { IngredientType, IngredientFormValues } from "../../types";
 import Button from "../../components/Button";
+import BackButton from "../../components/BackButton";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
 const useStyles = createUseStyles({
+  wrapper: {
+    position: "absolute",
+    backgroundColor: "rgba(13, 0, 0, 0.4)",
+    width: "100%",
+    height: "100%",
+  },
   header: {
     display: "flex",
-    marginTop: 30,
-  },
-  backButton: {
-    background: "transparent",
-    border: "none",
-    width: 80,
+    fontFamily: "'Reggae One', cursive",
+    color: "#F2E30C",
+    alignItems: "center",
+    paddingTop: 30,
+    marginLeft: "20%",
   },
   title: {
-    position: "relative",
-    left: -20,
-    width: "100%",
-    textAlign: "center",
+    marginLeft: "1rem",
   },
   formRoot: {
     display: "flex",
@@ -30,21 +39,51 @@ const useStyles = createUseStyles({
     alignItems: "center",
     width: "60%",
     margin: [0, "auto"],
+    borderRadius: 10,
+    backgroundColor: "rgba(252, 223, 135, 0.9)",
+    color: "#0D0000",
+    boxShadow: ["inset", 0, 0, 15, "#F99938"],
+    fontFamily: "'Catamaran', sans-serif",
+    padding: [0, "2rem", "1rem"],
+    boxSizing: "border-box",
   },
   fieldLabel: {
     fontSize: "1.3rem",
-    margin: [0, 0, 5],
+    margin: [15, 0, 5],
     alignSelf: "start",
   },
   formField: {
     width: "100%",
-    margin: [5, 0, 25],
+    margin: [5, 0, 10],
+    height: "2rem",
+    borderRadius: 10,
+    padding: [2, "1rem"],
+    fontSize: 16,
+    backgroundColor: "rgba(252, 240, 180, 0.8)",
+    color: "#0D0000",
+    border: 0,
+    boxSizing: "border-box",
+  },
+  formError: {
+    color: "#d93d1a",
+    alignSelf: "flex-start",
+    paddingLeft: "0.5rem",
+  },
+  submitButton: {
+    backgroundColor: "transparent",
+    border: "none",
+    borderRadius: 7,
+    fontSize: 16,
+    margin: [0, 4],
+    padding: ["0.5rem", "1rem"],
+    boxShadow: ["inset", 0, 0, 5, "#F99938"],
+    display: "flex",
+    alignItems: "center",
   },
 });
 
 const CreateIngredient = (): JSX.Element => {
   const classes = useStyles();
-  const navigate = useNavigate();
   const [typeList, setTypeList] = useState<IngredientType[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
@@ -65,11 +104,9 @@ const CreateIngredient = (): JSX.Element => {
   }, []);
 
   return (
-    <div>
+    <div className={classes.wrapper}>
       <header className={classes.header}>
-        <button className={classes.backButton} onClick={() => navigate(-1)}>
-          {"<< Back"}
-        </button>
+        <BackButton />
         <h1 className={classes.title}>Add a New Ingredient</h1>
       </header>
       {!loading ? (
@@ -104,41 +141,66 @@ const CreateIngredient = (): JSX.Element => {
             }
           }}
         >
-          <Form className={classes.formRoot}>
-            <p className={classes.fieldLabel}>Name</p>
-            <Field
-              className={classes.formField}
-              type="text"
-              name="ingredient_name"
-              label="Name"
-            />
-            <ErrorMessage name="ingredient_name" component="div" />
-            <p className={classes.fieldLabel}>Type</p>
-            <Field
-              className={classes.formField}
-              as="select"
-              name="ingredient_type_id"
-              label="Type"
-            >
-              {typeList?.length
-                ? typeList.map((type) => (
-                    <option key={type.id} value={type.id}>
-                      {type.ingredient_type_name}
-                    </option>
-                  ))
-                : "Could not retrieve type list"}
-            </Field>
-            <ErrorMessage name="ingredient_type_id" component="div" />
-            <p className={classes.fieldLabel}>Suggestions</p>
-            <Field
-              className={classes.formField}
-              type="textarea"
-              name="suggestions"
-              label="Suggestions"
-              placeholder="Suggest some brands or items"
-            />
-            <Button type="submit">Submit</Button>
-          </Form>
+          {({ isValid }: FormikProps<IngredientFormValues>): JSX.Element => (
+            <Form className={classes.formRoot}>
+              <label htmlFor="ingredient_name" className={classes.fieldLabel}>
+                Name
+              </label>
+              <Field
+                className={classes.formField}
+                type="text"
+                name="ingredient_name"
+                label="Name"
+              />
+              <ErrorMessage
+                className={classes.formError}
+                name="ingredient_name"
+                component="div"
+              />
+              <label
+                htmlFor="ingredient_type_id"
+                className={classes.fieldLabel}
+              >
+                Type
+              </label>
+              <Field
+                className={classes.formField}
+                as="select"
+                name="ingredient_type_id"
+                label="Type"
+              >
+                {typeList?.length
+                  ? typeList.map((type) => (
+                      <option key={type.id} value={type.id}>
+                        {type.ingredient_type_name}
+                      </option>
+                    ))
+                  : "Could not retrieve type list"}
+              </Field>
+              <ErrorMessage
+                className={classes.formError}
+                name="ingredient_type_id"
+                component="div"
+              />
+              <label htmlFor="suggestions" className={classes.fieldLabel}>
+                Suggestions
+              </label>
+              <Field
+                className={classes.formField}
+                type="textarea"
+                name="suggestions"
+                label="Suggestions"
+                placeholder="Suggest some brands or items"
+              />
+              <Button
+                type="submit"
+                className={classes.submitButton}
+                disabled={!isValid}
+              >
+                Submit
+              </Button>
+            </Form>
+          )}
         </Formik>
       ) : null}
     </div>
